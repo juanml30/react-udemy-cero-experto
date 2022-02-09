1d6eef05d7a25bf0d8e98bfb03dffaac7f928fb6
juanml30 <moreno.l.juan@gmail.com>
2022-02-09 01:28:10 -0300

    arreglos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Amando el hola mundo");
+const arreglo = [1,2,3,4]
+
+let arreglo2 = [...arreglo,5]
+
+
+
+const arreglo3= arreglo2.map( (element) => element+5 )
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
