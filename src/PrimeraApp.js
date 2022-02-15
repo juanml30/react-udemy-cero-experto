3f13b3dbb03569c598fabf8c2bf04f3311ffd92c
juanml30 <moreno.l.juan@gmail.com>
2022-02-15 01:28:15 -0300

    Primer componente

---
+++ src/PrimeraApp.js
@@ -0,0 +1,7 @@
+//Functional Components
+
+const FirstApp = () => {
+    return <h1>Hello World</h1>;
+}
+
+export default FirstApp
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Terminado el repaso")
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import "./index.css"
+
+const Root = document.querySelector("#root");
+
+ReactDOM.render(<PrimeraApp />, Root);
