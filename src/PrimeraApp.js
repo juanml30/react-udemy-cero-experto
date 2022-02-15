d93cf8305ee7f41b346a3740fd3c3c73b9588fae
juanml30 <moreno.l.juan@gmail.com>
2022-02-15 10:56:37 -0300

    concepto de imprimir objeto y usar json.stringify

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,10 +1,17 @@
 //Functional Components
-import React from "react";
 
 const PrimeraApp = () => {
+  const saludo1 = "Hola mundo";
+
+  const saludo2 = {
+    nombre: "Juan",
+    edad: 31,
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>{saludo1}</h1>
+      <pre> {JSON.stringify(saludo2)}</pre>
       <p>Mi primera aplicacion</p>
     </>
   );
