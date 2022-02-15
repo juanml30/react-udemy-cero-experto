9c6fbdd1c611520b91cf0efa08ce21b6fb7d7e81
juanml30 <moreno.l.juan@gmail.com>
2022-02-15 15:51:59 -0300

    Tarea generando un nuevo componente

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PrimeraApp from "./PrimeraApp";
+/* import CounterApp from "./CounterApp"; */
 import "./index.css";
+import CounterApp from "./CounterApp";
 
 const app = document.querySelector("#app");
 
-ReactDOM.render(<PrimeraApp saludo="Hola mundo" />, app);
+ReactDOM.render(<CounterApp value={13} />, app);
