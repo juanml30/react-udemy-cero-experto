6ccf08b81d0d2d82ca1d874b00e071a16202fff0
juanml30 <moreno.l.juan@gmail.com>
2022-02-16 12:34:49 -0300

    Creados 3 botones encargados de sumar-restar y reset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,11 +2,19 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
-  const handleAdd = (e) => {
+  const handleAdd = () => {
     setCounter(counter + 1);
     //setCounter((c) => c+1)
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+
+  const handleSustract = () => {
+    setCounter(counter - 1);
   };
 
   return (
@@ -14,6 +22,8 @@ const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSustract}>-1</button>
     </>
   );
 };
