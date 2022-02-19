692b8d92a94eec1e043f993d1e32ff0c6656f670
juanml30 <moreno.l.juan@gmail.com>
2022-02-19 11:58:20 -0300

    Creado el test que iguala objetos

---
+++ src/tests/base/05-funciones.test.js
@@ -0,0 +1,21 @@
+import { getUser, getUsuarioActivo } from "../../base/05-funciones";
+
+describe("Pruebas en 05-funciones", () => {
+  test("getuser debe de retornar un objeto", () => {
+    const userTest = { uid: "ABC123", username: "El_Papi1502" };
+    const user = getUser();
+    console.log(user);
+
+    expect(user).toStrictEqual(userTest);
+  });
+  test("getusuarioactivo debe de retornar un objeto", () => {
+    const nombre = "Juan"
+    const userTest = {
+      uid: "ABC567",
+      username: nombre,
+    };
+    const user = getUsuarioActivo(nombre)
+    console.log(user);
+    expect(user).toStrictEqual(userTest)
+  });
+});
