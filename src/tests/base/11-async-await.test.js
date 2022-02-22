01a5dc9dfdd72b56fadd46757ddc7addc63d6b4e
juanml30 <moreno.l.juan@gmail.com>
2022-02-22 10:54:09 -0300

    Pruebas de async await

---
+++ src/tests/base/11-async-await.test.js
@@ -0,0 +1,9 @@
+import { getImagen } from "../../base/11-async-await"
+
+describe("Pruebas con Async await",()=> {
+    test("debe retornar el url de la imagen", async()=>{
+        const url = await getImagen()
+        expect( url.includes("https://")).toBe(true)
+        console.log(url);
+    })
+})
